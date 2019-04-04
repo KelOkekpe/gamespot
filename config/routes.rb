8895e46c21ccb104e8a90036ebe5dbdd388e2cd4
joseph@callrail.com <jokekpe@MacBook-Pro-106.localdomain>
2019-04-04 16:16:27 -0400

    Change signup page styles, add simpler route to navigate to signup page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,12 @@ Rails.application.routes.draw do
   get '/pages/dashboard', to: 'pages#dashboard', as: 'dashboard'
 
   devise_scope :user do
-  get 'login', to: 'devise/sessions#new'
-end
+    get 'login', to: 'devise/sessions#new'
+  end
 
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+  end
+
+  # devise_for :users, :controllers => {:registrations => "registrations"}
 end
