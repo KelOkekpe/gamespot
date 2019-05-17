5b9ff80fd10a01c99ce43b0a709b9b4df07508ad
joseph@callrail.com <jokekpe@MacBook-Pro-106.localdomain>
2019-05-17 12:25:24 -0400

    Add ModalResponder service which modifies render and redirect_to methods which are defined in ActionController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,19 @@
 class ApplicationController < ActionController::Base
 
+protect_from_forgery with: :exception
+
     def redirect_if_not_signed_in
     redirect_to root_path if !user_signed_in?
   end
 
   def redirect_if_signed_in
     redirect_to root_path if user_signed_in?
+  end
+
+  def respond_modal_with(*args, &blk)
+    options = args.extract_options!
+    options[:responder] = ModalResponder
+    respond_with *args, options, &blk
   end
 
 end
