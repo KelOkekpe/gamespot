8c78ee2e06cf98827fc2c34ae3ab0f9fa51221dd
joseph@callrail.com <jokekpe@MacBook-Pro-106.local>
2020-02-07 17:35:42 -0500

    setup-backend-for-units-and-bookings

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :posts, dependent: :destroy
 
   has_many :host_bookings, :class_name => 'Booking', :foreign_key => 'host_id'
   has_many :cleaner_bookings, :class_name => 'Booking', :foreign_key => 'cleaner_id'
-  
+  has_many :units, :class_name => 'Unit', :foreign_key => 'owner_id'
 
   TYPES=[
     ['host','Host'],
