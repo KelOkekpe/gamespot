a03133177c2589a8384ddce4e8dfe4441f9928fc
KelOkekpe <joseph@callrail.com>
2020-02-12 12:27:21 -0500

    refactor dashboard partial to render components from pages helper

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,24 +1,34 @@
 module PagesHelper
 
-  def upcoming_bookings_partial_path
+  def upcoming_bookings_card_partial_path
     @user = current_user
-
     if current_user.user_type == 'host'
       @upcoming_bookings =  Booking.where(host_id:current_user.id).map{|booking| booking}
     elsif current_user.user_type == 'cleaner'
         @upcoming_bookings =  Booking.where(cleaner_id:current_user.id).map{|booking| booking}
     end
-    'pages/dashboard/cards/upcoming_bookings'
+    'pages/dashboard/cards/upcoming_bookings_card'
   end
 
 
+  def earnings_card_partial_path
+    'pages/dashboard/cards/earnings_card'
+  end
+
+  def pending_bookings_card_partial_path
+    'pages/dashboard/cards/pending_bookings_card'
+  end
+
+  def approved_bookings_card_partial_path
+    'pages/dashboard/cards/approved_bookings_card'
+  end
+
+  def calendar_card_partial_path
+    'pages/dashboard/cards/calendar_card'
+  end
+
   def dashboard_helper_path
-   @user = current_user
-   if @user.user_type == 'host'
      render 'pages/dashboard/dashboard'
-    elsif @user.user_type == 'cleaner'
-      render 'pages/dashboard/cleaner_dashboard'
-    end
   end
 
   # def user_table_helper_path
