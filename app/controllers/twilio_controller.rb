37abc56584a8e1c2a6d679507455753c5dd1d1b9
KelOkekpe <joseph@callrail.com>
2020-02-25 13:25:26 -0500

    invisible changes

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,6 +1,5 @@
 class TwilioController < ApplicationController
   skip_before_action :verify_authenticity_token
-
     # def sms
     #   response = Twilio::TwiML::MessagingResponse.new do |r|
     #     r.message body: Booking.last.notes
