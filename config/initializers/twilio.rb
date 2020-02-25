9d47335b03b0d718710fdf070511b3ba6c9e2fc7
KelOkekpe <joseph@callrail.com>
2020-02-25 13:07:25 -0500

    create ENV VAR for twilio account sid and auth token

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,4 +1,4 @@
 Twilio.configure do |config|
-    config.account_sid = 'AC59346a2425140b9bcc5e8f9c7f8abf1a'
-    config.auth_token = 'd95d60685ff7ac1b45daeeb15613a482'
+    ENV['TWILIO_SID']
+    ENV['TWILIO_AUTH_TOKEN']
 end
