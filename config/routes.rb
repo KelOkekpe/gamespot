e613690f6fc4b2774997642abdf6a676716f4f55
KelOkekpe <joseph@callrail.com>
2020-02-26 00:04:34 -0500

    comment devise in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :units
-  devise_for :users
+  # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
@@ -28,13 +28,13 @@ Rails.application.routes.draw do
 
 
   #Devise routes for logging in, registrations
-  devise_scope :user do
-    get 'login', to: 'devise/sessions#new'
-  end
+  # devise_scope :user do
+  #   get 'login', to: 'devise/sessions#new'
+  # end
 
-  devise_scope :user do
-    get 'signup', to: 'devise/registrations#new'
-  end
+  # devise_scope :user do
+  #   get 'signup', to: 'devise/registrations#new'
+  # end
 
 
 
