43e6f44d1610a3d2c88156aec95eb2e763e7c1a3
KelOkekpe <joseph@callrail.com>
2020-03-20 16:47:19 -0400

    change delete to destroy in routes

--- config/routes.rb
+++ config/routes.rb
@@ -45,7 +45,7 @@ Rails.application.routes.draw do
   end
 
   devise_scope :user do
-    get '/users/sign_out', to: 'devise/sessions#delete', as:'get_destroy_user_session'
+    get '/users/sign_out', to: 'devise/sessions#destroy', as:'get_destroy_user_session'
   end
 
 
